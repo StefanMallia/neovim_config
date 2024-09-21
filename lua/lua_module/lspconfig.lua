0b8991d73c6f4c6c446f4ae768551bf8932340ff
StefanMallia <5004438+StefanMallia@users.noreply.github.com>
2024-09-21 11:55:27 +0200

    Add lsp config

---
+++ lua/lua_module/lspconfig.lua
@@ -0,0 +1,19 @@
+require("lspconfig").clangd.setup({
+  on_attach = function()
+  end
+})
+
+require'lspconfig'.pylsp.setup{}
+
+local isLspDiagnosticsVisible = true
+vim.keymap.set("n", "<leader>lx", function()
+    isLspDiagnosticsVisible = not isLspDiagnosticsVisible
+    vim.diagnostic.config({
+        virtual_text = isLspDiagnosticsVisible,
+        underline = isLspDiagnosticsVisible
+    }) end)
+
+vim.keymap.set("n", "<leader>lj", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>lk", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "<leader>lo", vim.diagnostic.open_float)
+
